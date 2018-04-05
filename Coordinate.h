1b536077c6fe8d9107cfe8a9eb2e2c28afa626f2
李健辉 <leekunhwee@gmail.com>
2018-04-05 15:31:09 +0800

    添加项目文件。

---
+++ Coordinate.h
@@ -0,0 +1,24 @@
+#pragma once
+class Coordinate
+{
+public:
+	Coordinate();
+	~Coordinate();
+
+public:
+	int m_iX;
+	int m_iY;
+
+//private:
+
+};
+
+/*
+Coordinate::Coordinate()
+{
+}
+
+Coordinate::~Coordinate()
+{
+}
+*/
